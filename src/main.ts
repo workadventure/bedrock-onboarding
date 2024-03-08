7e7fdc72305174e6cbadb218f490f0c03efed7c3
Valdo <vteixeiraromao@gmail.com>
2024-03-08 16:35:52 +0000

    Add entry/exits + move camera to roof

--- src/main.ts
+++ src/main.ts
@@ -4,33 +4,23 @@ import { bootstrapExtra } from "@workadventure/scripting-api-extra";
 
 console.log('Script started successfully');
 
-let currentPopup: any = undefined;
-
 // Waiting for the API to be ready
-WA.onInit().then(() => {
+WA.onInit().then(async () => {
     console.log('Scripting API ready');
     console.log('Player tags: ',WA.player.tags)
 
-    WA.room.area.onEnter('clock').subscribe(() => {
-        const today = new Date();
-        const time = today.getHours() + ":" + today.getMinutes();
-        currentPopup = WA.ui.openPopup("clockPopup", "It's " + time, []);
+    const roofArea = await WA.room.area.get("roof");
+
+    WA.room.area.onEnter('to-tour-rooftop').subscribe(() => {
+        WA.nav.goToRoom("#roof")
+        WA.camera.set(roofArea.x, roofArea.y, 1000, 1000, false, true);
     })
-
-    WA.room.area.onLeave('clock').subscribe(closePopup)
-
+        
     // The line below bootstraps the Scripting API Extra library that adds a number of advanced properties/features to WorkAdventure
     bootstrapExtra().then(() => {
         console.log('Scripting API Extra ready');
     }).catch(e => console.error(e));
 
 }).catch(e => console.error(e));
-
-function closePopup(){
-    if (currentPopup !== undefined) {
-        currentPopup.close();
-        currentPopup = undefined;
-    }
-}
 
 export {};
