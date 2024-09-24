7077c9c9610dce59a80301c8b0986b0eb4504932
ValdoTR <vteixeiraromao@gmail.com>
2024-09-24 17:47:16 +0000

    Deploying to gh-pages from master @ 6d9124f1555b9f0a8f7499884909fb2fb50fb05b 🚀

---
+++ assets/Maps-34277f5b.js
@@ -0,0 +1,1 @@
+const o="/@/bedrock-1710774685/onboardingbr",r={town:`${o}/town`,world:`${o}/e-learning`},l=["ext","0","1","2","3","4","roof"],n={roof:null,4:{x:31,y:124},3:{x:29,y:129},2:{x:29,y:137},1:{x:17,y:143},0:{x:28,y:150},ext:null};export{l as b,n as f,r as m};
